4e8f85af8d73477af3d8bdced25363b8c5915bdb
user19166211 <19166211@brookes.ac.uk>
2025-07-02 23:23:40 +0900

    add start/quit

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ book = askopenfilename()
 pdfreader = PyPDF2.PdfReader(book) 
 pages = len(pdfreader.pages) # returns number of pages in the pdf
 
+player = pyttsx3.init()
+
 #read all data in PDF pages
 for num in range(0,pages):
     page = pdfreader.pages[num] #gets the page at the number 'num', returned as an object
     text = page.extract_text() #extracts plaintext of the page
-    player = pyttsx3.init()
-    player.say(text)
-    player.runAndWait()
+
+
+    if text:
+        print(f"Reading page {num + 1}/{pages}. Press Enter to continue or type 'q' to quit.")
+        usrinput = input()
+
+        if usrinput.lower() == 'q':
+            break
+
+        player.say(text)
+        player.runAndWait()
